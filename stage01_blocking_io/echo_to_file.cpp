946fcd98fd90f7bbd5e9aa3daf824ef794f026d9
mrunmoy <mrunmoy@gmail.com>
2025-08-01 10:59:05 +1000

    enabling multiline inputs

--- stage01_blocking_io/echo_to_file.cpp
+++ stage01_blocking_io/echo_to_file.cpp
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <vector>
 
 #define STD_INPUT_FD 0
 #define STD_OUTPUT_FD 1
 
 int main(int argc, char *argv[])
 {
-    int outFileFD = open("outfile.txt", O_WRONLY | O_CREAT, 0644);
+    int outFileFD = open("outfile.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (outFileFD < 0)
     {
         std::cerr << "Error opening file for writing." << std::endl;
@@ -24,22 +25,45 @@ int main(int argc, char *argv[])
         // Prompt user for input
         write(STD_OUTPUT_FD, userPrompt.c_str(), userPrompt.size());
 
-        // Read input from standard input
-        int bytesRead = read(STD_INPUT_FD, userInput, sizeof(userInput));
-        if (!bytesRead)
+        std::vector<std::string> userStrings;
+
+        enum class Command { EndInput, Quit, Continue };
+        Command cmd = Command::Continue;
+        while (true)
         {
-            write(STD_OUTPUT_FD, "Read error or EOF.\n", 20);
-            continue; // Skip to the next iteration if input is empty
+            // Read input from standard input
+            int bytesRead = read(STD_INPUT_FD, userInput, sizeof(userInput) - 1);
+            if (!bytesRead)
+            {
+                write(STD_OUTPUT_FD, "Read error or EOF.\n", 20);
+                continue; // Skip to the next iteration if input is empty
+            }
+
+            userInput[bytesRead] = '\0';
+            std::string inputStr(userInput);
+
+            if (inputStr == "quit\n" || inputStr == "exit\n")
+            {
+                cmd = Command::Quit;
+                break;
+            }
+            else if (inputStr == "---END---\n")
+            {
+                cmd = Command::EndInput; // End input on empty line
+                break;
+            }
+
+            userStrings.push_back(inputStr);
         }
 
-        userInput[bytesRead] = '\0';
-        std::string inputStr(userInput);
-
-        if (inputStr == "quit\n" || inputStr == "exit\n")
+        if (cmd == Command::Quit)
             break;
 
         // Write the input to a file
-        write(outFileFD, userInput, bytesRead);
+        for (const auto &str : userStrings)
+        {
+            write(outFileFD, str.c_str(), str.size());
+        }
     }
 
     close(outFileFD);
